c1d6e6f45ff6d3ee12ae9b6840ceef9684e1e3c4
Prafull Sharma <prafull.sharma@ekino.com>
2018-02-21 14:32:43 +0530

    signup flow with sagas and reducer and you can do signup process now

--- src/signup/reducer.js
+++ src/signup/reducer.js
@@ -1,4 +1,8 @@
-import { SIGNUP_REQUESTING } from './constants'
+import {
+  SIGNUP_REQUESTING,
+  SIGNUP_SUCCESS,
+  SIGNUP_ERROR,
+} from './constants'
 
 const initialState = {
   requesting: false,
@@ -15,6 +19,35 @@ const reducer = function signupReducer (state = initialState, action) {
         successful: false,
         messages: [{ body: 'Signing up...', time: new Date() }],
         errors: [],
+      }
+
+    // reset the state and add a body message of success!
+    // remember our successful returned payload will be:
+    // {"email": "of the new user", "id": "of the user"}
+    case SIGNUP_SUCCESS:
+      return {
+        errors: [],
+        messages: [{
+          body: `Successfully created account for ${action.response.email}`,
+          time: new Date(),
+        }],
+        requesting: false,
+        successful: true,
+      }
+
+    // reset the state but with errors!
+    // the error payload returned is actually far
+    // more detailed, but we'll just stick with
+    // the base message for now
+    case SIGNUP_ERROR:
+      return {
+        errors: state.errors.concat([{
+          body: action.error.toString(),
+          time: new Date(),
+        }]),
+        messages: [],
+        requesting: false,
+        successful: false,
       }
 
     default:
