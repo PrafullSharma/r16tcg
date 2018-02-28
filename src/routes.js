e067ba177b7fc8e18d1b5598a93674467706f7a0
Prafull Sharma <prafull.sharma@ekino.com>
2018-02-28 15:06:28 +0530

    docker setup here

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import Navigation from './common/Navigation'
 import Home from './home/index'
 import Login from './login/index'
 import Signup from './signup/index'
-// import Widgets from './widgets/index'
+import Widgets from './widgets/index'
 import NotFoundPage from './common/NotFoundPage'
 
 const Routes = () => (
@@ -21,6 +21,7 @@ const Routes = () => (
         <Route exact path="/" component={Home}/>
         <Route path="/login" component={Login}/>
         <Route path="/signup" component={Signup}/>
+        <Route path="/widgets" component={Widgets}/>
         {/*<PrivateRoute path="/widgets" redirectTo={'/login'} component={Widgets} />*/}
         <Route component={NotFoundPage} />
       </Switch>
